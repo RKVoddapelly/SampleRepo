74c3ac45bb5667e55f1a37aa739f74fcd0a07421
RKVoddapelly <rupesh@guruofjava.com>
2018-08-14 17:30:13 +0530

    Update Sample.java

--- SampleGitApp/src/main/java/Sample.java
+++ SampleGitApp/src/main/java/Sample.java
@@ -11,5 +11,6 @@
 public class Sample {
     public static void main(String... args){
         System.out.println("Welcome to Git Learning");
+        System.out.println("Self Learning Continues");
     }
 }
